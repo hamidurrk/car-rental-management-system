203cbc56ddb68c3aac8f4bbdefad9eddca289f27
hamidurrk <hamidurrk@gmail.com>
2024-11-16 21:47:21 +0200

    version 1 done including all functionalities. testing and debugging not done

--- src/project.py
+++ src/project.py
@@ -17,7 +17,6 @@ class CarRentalManagementSystem:
         self.RENTED_VEHICLES_FILE = "files/rentedVehicles.txt"
         self.TRANSACTION_FILE = "files/transActions.txt"
         
-        self.open_files()
         self.theme = {
             "title": "magenta",
             "info": "blue",
@@ -34,69 +33,52 @@ class CarRentalManagementSystem:
                 "0": "Exit"
             }
         self.available_cars_table_header = ["Reg. No.", "Model", "Price/Day", "Features"]
-        
-    def __del__(self):
-        self.close_files()
     
-    def open_files(self):
-        self.vehicle_file = open(self.VEHICLES_FILE, "r")
-        self.customer_file = open(self.CUSTOMERS_FILE, "a+")
-        self.rented_vehicle_file = open(self.RENTED_VEHICLES_FILE, "a+")
-        self.transaction_file = open(self.TRANSACTION_FILE, "a+")
-    
-    def close_files(self):
-        self.customer_file.close()
-        self.vehicle_file.close()
-        self.rented_vehicle_file.close()
-        self.transaction_file.close()
+# -------------- Data handling --------------
     
     def truncate_file(self, file_name):
         file = open(file_name, "w")
         file.truncate(0)
         file.close()
     
-    def reinitialize(self):
-        self.close_files()
-        self.open_files()
-
-# -------------- Data handling --------------
-    
     def get_data(self, file):
         data = []
-        file = file.readlines()
-        for line in file:
+        file_data = file.readlines()
+        for line in file_data:
             data.append(line.strip().split(","))
+        file.close()
         return data
         
     def get_vehicle_data(self):
-        self.reinitialize()
+        self.vehicle_file = open(self.VEHICLES_FILE, "r+")
         return self.get_data(self.vehicle_file)
     
     def get_customer_data(self):
-        self.reinitialize()
+        self.customer_file = open(self.CUSTOMERS_FILE, "r")
         return self.get_data(self.customer_file)
     
     def get_rented_vehicle_data(self):
-        self.reinitialize()
+        self.rented_vehicle_file = open(self.RENTED_VEHICLES_FILE, "r")
         return self.get_data(self.rented_vehicle_file)
     
     def get_transaction_data(self):
-        self.reinitialize()
+        self.transaction_file = open(self.TRANSACTION_FILE, "r")
         return self.get_data(self.transaction_file)
   
     def write_data(self, file, data):
         file.write(data)
+        file.close()
     
     def write_customer_data(self, data):
-        self.reinitialize()
+        self.customer_file = open(self.CUSTOMERS_FILE, "a+")
         self.write_data(self.customer_file, data)
     
     def write_rented_vehicle_data(self, data):
-        self.reinitialize()
+        self.rented_vehicle_file = open(self.RENTED_VEHICLES_FILE, "a+")
         self.write_data(self.rented_vehicle_file, data)
         
     def write_transaction_data(self, data):
-        self.reinitialize()
+        self.transaction_file = open(self.TRANSACTION_FILE, "a+")
         self.write_data(self.transaction_file, data)
                 
     def list_to_csv(self, data: list):
@@ -110,15 +92,18 @@ class CarRentalManagementSystem:
             
     def remove_data_from_rented_vehicles(self, license_plate: str):
         rented_vehicles = self.get_rented_vehicle_data()
-        print(rented_vehicles)
+        # print(rented_vehicles)
         for i, line in enumerate(rented_vehicles):
             if license_plate in line:
                 del rented_vehicles[i]
                 break    
         self.truncate_file(self.RENTED_VEHICLES_FILE)
         for vehicle in rented_vehicles:
-            print(self.list_to_csv(vehicle))
+            # print(self.list_to_csv(vehicle))
             self.write_rented_vehicle_data(self.list_to_csv(vehicle))
+
+    def date_obj(self, date: str):
+        return datetime.strptime(date, "%d/%m/%Y %H:%M")
 
 # -------------- Interface --------------     
  
@@ -274,25 +259,51 @@ class CarRentalManagementSystem:
             self.println(self.print_error("Email format not correct."), adjust=9)
             self.println(self.print_warning("Please enter a valid email address."), adjust=9)
             return False
+        
+        if len(email.split("@")) > 2:
+            self.println(self.print_error("Email format not correct."), adjust=9)
+            self.println(self.print_warning("Please enter a valid email address."), adjust=9)
+            return False
+        
         email_domain = email.split("@")[1]
         if "." not in email_domain:
             self.println(self.print_error("Email domain not correct."), adjust=9)
             self.println(self.print_warning("Please enter a valid email address."), adjust=9)
             return False
+        
         email_domain = email_domain.split(".")
-        for parts in email_domain:
-            if not parts.isalpha():
-                if not parts.isdigit():
+        if len(email_domain) <= 1:
+            self.println(self.print_error("Email domain not correct."), adjust=9)
+            self.println(self.print_warning("Please enter a valid email address."), adjust=9)
+            return False
+        else:
+            for parts in email_domain:
+                if len(parts) <= 0:
                     self.println(self.print_error("Email domain not correct."), adjust=9)
                     self.println(self.print_warning("Please enter a valid email address."), adjust=9)
                     return False
+                if not parts.isalpha():
+                    if not parts.isdigit():
+                        self.println(self.print_error("Email domain not correct."), adjust=9)
+                        self.println(self.print_warning("Please enter a valid email address."), adjust=9)
+                        return False
+                    
         email_username = email.split("@")[0]
         allowed_characters = "._"
         
+        if allowed_characters in email_username or allowed_characters[::-1] in email_username:
+            self.println(self.print_error("Email username not correct."), adjust=9)
+            self.println(self.print_warning("Please enter a valid email address."), adjust=9)
+            return False
+        
         for char in allowed_characters:
+            if char == email_username[0] or char == email_username[-1]:            
+                self.println(self.print_error("Email username not correct."), adjust=9)
+                self.println(self.print_warning("Please enter a valid email address."), adjust=9)
+                return False
             if char in email_username:
-                email_username = email_username.replace(char, "")
-                
+                email_username = email_username.replace(char, "") 
+        
         if not email_username.isalnum():            
             self.println(self.print_error("Email username not correct."), adjust=9)
             self.println(self.print_warning("Please enter a valid email address."), adjust=9)
@@ -354,6 +365,7 @@ class CarRentalManagementSystem:
                     self.println(self.print_title("Welcome to our car rental service!"),center=True)
                     self.print_bar("-")
                     self.println(self.print_info("Please register with your information below:"), adjust=9)
+                    self.println(self.print_warning("By registering you are consenting to store your personal information in our database."), adjust=9)
                     new_customer_info.append(input_birthday.strftime("%d/%m/%Y"))
                     is_new_customer = True
             else:
@@ -395,14 +407,70 @@ class CarRentalManagementSystem:
             self.println(self.print_success(f"Thank you choosing us!"), adjust=9)    
         
 # --------------- Menu option 3 -----------------
-
+    
+    def check_rent_calculation(self):
+        transactions = self.get_transaction_data()
+        for transaction in transactions:
+            vehicle_info = self.search_data(self.get_vehicle_data(), transaction[0])
+            rent_per_day = int(vehicle_info[2])
+            
+            start_date = self.date_obj(transaction[2])
+            end_date = self.date_obj(transaction[3])
+            total_days = (end_date - start_date).days + 1
+            rent = rent_per_day * total_days
+            print(rent)
+    
+    def calculate_and_parse_rent_details(self, reg_no):
+        vehicle_info = self.search_data(self.get_vehicle_data(), reg_no)
+        vehicle_rent_info = self.search_data(self.get_rented_vehicle_data(), reg_no)
+        rent_per_day = int(vehicle_info[2])
+        start_date = self.date_obj(vehicle_rent_info[2])
+        end_date = datetime.now()
+        total_days = (end_date - start_date).days + 1
+        rent = rent_per_day * total_days
+        return total_days, start_date, end_date, rent
+        
     def return_a_car(self):
-        pass
+        is_car_rented = False
+        self.print_section_title(title="Return a car")
+        input_car_no = self.take_input("Enter the car registration number: ")
+        if self.search_data(self.get_vehicle_data(), input_car_no) == None:
+            self.println(self.print_error("Car not owned by the company."), adjust=9)
+        else:
+            if self.search_data(self.get_rented_vehicle_data(), input_car_no) == None:
+                self.println(self.print_error(f"Car with registration number {input_car_no} is not rented."), adjust=9)
+            else:
+                self.println(self.print_success(f"The car was rented on {self.search_data(self.get_rented_vehicle_data(), input_car_no)[2]}"), adjust=9)
+                is_car_rented = True
+
+        if is_car_rented:
+            transaction_query = []
+            rented_vehicle_info = self.search_data(self.get_rented_vehicle_data(), input_car_no)
+            total_rented_days, start_date, end_date, bill = self.calculate_and_parse_rent_details(input_car_no)
+            bill = float(bill)
+            transaction_query.append(input_car_no)
+            transaction_query.append(rented_vehicle_info[1])
+            transaction_query.append(start_date.strftime("%d/%m/%Y %H:%M"))
+            transaction_query.append(end_date.strftime("%d/%m/%Y %H:%M"))
+            transaction_query.append(str(total_rented_days))
+            transaction_query.append(f"{bill:.2f}")
+            self.println(self.print_success(f"Total rented days: {total_rented_days}"), adjust=9)
+            self.println(self.print_success(f"Bill: {bill:.2f} euros"), adjust=9)
+            self.write_transaction_data(self.list_to_csv(transaction_query))
+            self.print_bar("-")
+            self.println(self.print_title(f"Thank you for renting with us!"), adjust=9)
+            self.print_bar("-")
+            self.remove_data_from_rented_vehicles(input_car_no)
 
 # --------------- Menu option 4 -----------------
-
+    
     def count_the_money(self):
-        pass
+        transactions = self.get_transaction_data()
+        revenue = []
+        for transaction in transactions:
+            revenue.append(float(transaction[5].replace(",", "")))
+        total_revenue = sum(revenue)
+        self.println(self.print_success(f"Total revenue: {total_revenue:.2f} euros"), adjust=9)
 
 # ----------------- Main Menu -------------------
 
@@ -416,7 +484,7 @@ class CarRentalManagementSystem:
             self.println(self.print_info(f"{key}) {value}"), adjust=9)
         while True:
             try:
-                option_choice = int(self.take_input("Enter a choice [0-4]: "))
+                option_choice = int(self.take_input("Enter your selection [0-4]: "))
                 option = self.menu_options[str(option_choice)]
                 break
             except ValueError:
@@ -430,22 +498,19 @@ class CarRentalManagementSystem:
         iteration_count = 0
         while True:
             option = self.menu(iteration_count)
-            match option:
-                case "List available cars":
-                    self.list_available_cars()
-                case "Rent a car":
-                    self.rent_a_car()
-                case "Return a car":
-                    pass
-                case "Count the money":
-                    pass
-                case "Exit":
-                    self.print_footer()
-                    break
+            if option == "List available cars":
+                self.list_available_cars()
+            elif option == "Rent a car":
+                self.rent_a_car()
+            elif option == "Return a car":
+                self.return_a_car()
+            elif option == "Count the money":
+                self.count_the_money()
+            elif option == "Exit":
+                self.print_footer()
+                break
             iteration_count += 1
         
 if __name__ == "__main__":
     system = CarRentalManagementSystem()
     system.main()
-    # system.rent_a_car()
-    # print(system.check_email(""))
